8553a4afe43c0d0e5bc5368374652a94b04cf8ae
dzmitry.anikeichanka <dzmitry.anikeichanka@devbridge.com>
2024-04-23 00:13:22 +0200

    Add AI model selection & split text match in MatchForm.jsx

--- src/features/match-form/MatchForm.jsx
+++ src/features/match-form/MatchForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import {useEffect, useState} from "react";
 import {
   Button,
   FormControl,
@@ -12,7 +12,9 @@ import {
   Text,
   Image,
   Wrap,
-  WrapItem,
+  SimpleGrid,
+  Select,
+  Checkbox
 } from "@chakra-ui/react";
 import { useForm } from "react-hook-form";
 import axios, { getBaseUrl } from "src/axios";
@@ -30,10 +32,13 @@ const MyForm = () => {
     },
   });
   const [response, setResponse] = useState("");
+  const [models, setModels] = useState([]);
+  const [selectedModel, setSelectedModel] = useState([]);
+  const [splitBySentences, setSplitBySentences] = useState(true)
 
   const onSubmit = async ({ text, count }) => {
     try {
-      const responseData = await axios.post("/memes/match", {
+      const responseData = await axios.post(`/memes/match?modelId=${selectedModel}&split=${splitBySentences}`, {
         text,
         count,
       });
@@ -42,6 +47,26 @@ const MyForm = () => {
       setResponse(error.message);
     }
   };
+
+  const handleModelChange = (event) => {
+    const modelId = event.target.value;
+    setSelectedModel(modelId);
+  };
+
+  const handleCheckboxChange = (event) => {
+    const split = event.target.checked;
+    setSplitBySentences(split)
+  };
+
+  useEffect(() => {
+    axios.get("/models")
+        .then((rs) => {
+          setModels(rs.data)
+        })
+        .catch((error) => {
+          console.error("Error fetching models:", error);
+        })
+  }, [])
 
   return (
     <BoxWithShadow>
@@ -64,6 +89,27 @@ const MyForm = () => {
           </NumberInput>
         </FormControl>
 
+        <FormControl>
+          <FormLabel htmlFor="model">Select model</FormLabel>
+          <Select
+              id="modelSelect"
+              placeholder="Select model"
+              onChange={handleModelChange}
+          >
+            {models.map((model) => (
+              <option key={model.id} value={model.id}>{model.name}</option>
+              )
+            )}
+          </Select>
+        </FormControl>
+
+        <FormControl>
+          <Checkbox
+              defaultChecked
+              onChange={handleCheckboxChange}
+          >Match by sentences</Checkbox>
+        </FormControl>
+
         <Button
           mt={4}
           colorScheme="teal"
@@ -73,21 +119,31 @@ const MyForm = () => {
           Submit
         </Button>
 
-        {response?.memes ? (
-          <Box mt={4}>
-            <Wrap spacing="20px">
-              {response?.memes?.map((meme) => (
-                <WrapItem key={meme.id}>
-                  <Image
-                    key={meme.id}
-                    boxSize="150px"
-                    src={getMemeImageUrl(meme)}
-                    alt={meme.id}
-                  />
-                </WrapItem>
-              ))}
-            </Wrap>
-          </Box>
+        {response?.sentences ? (
+            <Box mt={4}>
+              <Wrap spacing="20px">
+                  {response?.sentences?.map((sentence) => (
+                      <SimpleGrid columns={2} spacingX='40px' spacingY='20px'>
+                        <Box mt={4}>
+                          <Text>{sentence.sentence}</Text>
+                        </Box>
+                        <br/>
+                        {sentence?.memes?.map((meme) => (
+                            <Box mt={4}>
+                              <Wrap spacing="20px">
+                                <Image
+                                    key={meme.id}
+                                    boxSize="150px"
+                                    src={getMemeImageUrl(meme)}
+                                    alt={meme.id}
+                                />
+                              </Wrap>
+                            </Box>
+                        ))}
+                      </SimpleGrid>
+                  ))}
+              </Wrap>
+            </Box>
         ) : (
           <Text>{response}</Text>
         )}
